4428fd83f75c8d765a1ce0d75e58176ddf84c747
Luke Reed <cpt.spleen@gmail.com>
2022-02-05 17:52:47 +1300

    Fix namespace for assembly helper

--- MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/IntermediateSerializer.cs
+++ MonoGame.Framework.Content.Pipeline/Serialization/Intermediate/IntermediateSerializer.cs
@@ -2,7 +2,7 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-using MonoGame.Utilities;
+using MonoGame.Framework.Utilities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
